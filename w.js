f95ce0ed1abe384cee74b042248ace5ff9c22057
skibiditoilet423 <redewarriorsmoney@gmail.com>
2024-10-20 10:43:54 -0300

    Update w.js

--- w.js
+++ w.js
@@ -1,6 +1,7 @@
     {
         "ips": [
             "45.164.125.95:25681",
+            "45.164.125.95:25702",
             "jogar.redewarriors.com",
             "redewarriors.com",
             "45.164.125.95"
